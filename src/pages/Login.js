eacb850b8b7aa9a4f08d358df3860e401577bbbb
Marcello Santoro <marcelloaltermundi@gmail.com>
2022-10-03 17:01:26 -0300

    adiciona requisitos 1 e 2

---
+++ src/pages/Login.js
@@ -0,0 +1,67 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import Carregando from '../services/Carregando';
+import { createUser } from '../services/userAPI';
+
+const obj = {
+  nameLogin: '',
+  buttonLogin: true,
+  renderLoading: false,
+};
+
+export default class Login extends Component {
+  state = { ...obj };
+
+  onInputChange = (event) => {
+    const { name, value } = event.target;
+    const THREE = 3;
+    this.setState({
+      [name]: value,
+      buttonLogin: (value.length < THREE),
+    });
+  };
+
+  onEnterButton = () => {
+    const { nameLogin } = this.state;
+    const { history } = this.props;
+    const { push } = history;
+    this.setState({ renderLoading: true }, async () => {
+      await createUser({ name: nameLogin });
+      push('/search');
+    });
+  };
+
+  render() {
+    const { buttonLogin, nameLogin, renderLoading } = this.state;
+    if (renderLoading) {
+      return <Carregando />;
+    }
+    return (
+      <div data-testid="page-login">
+        <input
+          type="text"
+          placeholder="Nome"
+          data-testid="login-name-input"
+          name="nameLogin"
+          value={ nameLogin }
+          onChange={ this.onInputChange }
+        />
+        <button
+          type="button"
+          data-testid="login-submit-button"
+          disabled={ buttonLogin }
+          onClick={ this.onEnterButton }
+        >
+          Entrar
+
+        </button>
+      </div>
+    );
+  }
+}
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
