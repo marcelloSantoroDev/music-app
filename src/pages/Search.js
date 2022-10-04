1432a2f1c05b5d61091b1746d5e5a705d27faa4a
Marcello Santoro <marcelloaltermundi@gmail.com>
2022-10-03 21:00:47 -0300

    adiciona requisito 6 com falha em dois testes

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Carregando from '../services/Carregando';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 const obj = {
   searchValue: '',
   searchButton: true,
+  loading: false,
+  responseValue: [],
+  flag: false,
 };
 
 export default class extends Component {
@@ -17,8 +23,19 @@ export default class extends Component {
     });
   };
 
+  handleFetch = () => {
+    const { searchValue } = this.setState;
+    this.setState({ loading: true, flag: true }, async () => {
+      const responseAPI = await searchAlbumsAPI(searchValue);
+      console.log(responseAPI);
+      this.setState({ responseValue: responseAPI }, () => {
+        this.setState({ loading: false });
+      });
+    });
+  };
+
   render() {
-    const { searchValue, searchButton } = this.state;
+    const { searchValue, searchButton, loading, responseValue, flag } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
@@ -32,14 +49,39 @@ export default class extends Component {
             onChange={ this.onInputChange }
           />
           <button
-            type="submit"
+            type="button"
             data-testid="search-artist-button"
             disabled={ searchButton }
+            onClick={ this.handleFetch } // => função recebe uma string com o nome da banda
           >
             Pesquisar
 
           </button>
         </form>
+        {loading && <Carregando />}
+        {flag && (
+          <p>
+            Resultado de álbuns de:
+            {' '}
+            {searchValue}
+          </p>
+        )}
+        {(responseValue.length === 0 && flag) ? (
+          <p>Nenhum álbum foi encontrado</p>)
+          : (
+            <ul>
+              {responseValue
+                .map((element, index) => (
+                  <Link
+                    data-testid={ `link-to-album-${element.collectionId}` }
+                    key={ index }
+                    to={ `/album/${element.collectionId}` }
+                  >
+                    {JSON.stringify(element)}
+
+                  </Link>))}
+            </ul>
+          )}
       </div>
     );
   }
