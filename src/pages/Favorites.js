f026facc9d39e924f2b76399d58e31f828cb3bbf
Marcello Santoro <marcelloaltermundi@gmail.com>
2022-10-05 11:14:49 -0300

    adiciona requisito 12 - sem passarno último teste

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+import Carregando from '../services/Carregando';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+
+const obj = {
+  favorites: [],
+  loading: true,
+};
 
 export default class extends Component {
+  state = { ...obj };
+
+  componentDidMount() {
+    this.handleFetch();
+  }
+
+  handleFetch = async () => {
+    const getFavs = await getFavoriteSongs();
+    console.log(getFavs);
+    this.setState({
+      favorites: [...getFavs],
+      loading: false,
+    });
+  };
+
   render() {
+    const { loading, favorites } = this.state;
+    if (loading) {
+      return <Carregando />;
+    }
     return (
       <div data-testid="page-favorites">
         <Header />
+        {favorites.map((element, index) => <MusicCard key={ index } { ...element } />)}
       </div>
     );
   }
